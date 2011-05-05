8c49617d62ce08ee90a5440c998ad74beb830f3e
Christoph Bruecke <christoph.bruecke@campus.tu-berlin.de>
2011-05-05 15:52:44 +0200

    Implemented ReduceSideJoin excercise

--- seminar/exercises/datamining/core/src/main/java/de/tuberlin/dima/aim/exercises/two/ReduceSideBookAndAuthorJoin.java
+++ seminar/exercises/datamining/core/src/main/java/de/tuberlin/dima/aim/exercises/two/ReduceSideBookAndAuthorJoin.java
@@ -18,8 +18,24 @@
 package de.tuberlin.dima.aim.exercises.two;
 
 import de.tuberlin.dima.aim.exercises.hadoop.HadoopJob;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
 
+import com.google.common.collect.ComparisonChain;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 import java.util.Map;
 
 public class ReduceSideBookAndAuthorJoin extends HadoopJob {
@@ -33,8 +49,150 @@ public class ReduceSideBookAndAuthorJoin extends HadoopJob {
     Path books = new Path(parsedArgs.get("--books"));
     Path outputPath = new Path(parsedArgs.get("--output"));
 
-    // IMPLEMENT ME
+    Job reduceSideJoin = new Job(new Configuration(getConf()), "ReduceSideJoin");
 
+    reduceSideJoin.setMapOutputKeyClass(JoinKey.class);
+    reduceSideJoin.setMapOutputValueClass(Text.class);
+    reduceSideJoin.setReducerClass(JoinReducer.class);
+    reduceSideJoin.setOutputKeyClass(Text.class);
+    reduceSideJoin.setOutputValueClass(Text.class);
+    reduceSideJoin.setOutputFormatClass(TextOutputFormat.class);
+    
+    reduceSideJoin.setPartitionerClass(JoinPartinioner.class);
+    reduceSideJoin.setGroupingComparatorClass(JoinGroupingComparator.class);
+    
+    reduceSideJoin.getConfiguration().set("mapred.output.dir", outputPath.toString());
+    
+    MultipleInputs.addInputPath(reduceSideJoin, authors, TextInputFormat.class, ProjectAuthorsMapper.class);
+    MultipleInputs.addInputPath(reduceSideJoin, books, TextInputFormat.class, ProjectBooksMapper.class);
+    
+    reduceSideJoin.waitForCompletion(true);
+    
     return 0;
+  }
+  
+  static class ProjectAuthorsMapper extends Mapper<Object, Text, JoinKey, Text> {
+	 
+	  @Override
+	  protected void map(Object key, Text line, Context ctx) throws java.io.IOException ,InterruptedException {
+		  String[] fields = line.toString().split("\t");
+		  int id = Integer.parseInt(fields[0]);
+		  String author = fields[1];
+		  
+		  ctx.write(new JoinKey(id, JoinKey.AUTHOR_TAG), new Text(author));
+	  }
+  }
+  
+  static class ProjectBooksMapper extends Mapper<Object, Text, JoinKey, Text> {
+	  
+	  @Override
+	  protected void map(Object key, Text line, Context ctx) throws java.io.IOException ,InterruptedException {
+		  String[] fields = line.toString().split("\t");
+		  int id = Integer.parseInt(fields[0]);
+		  int year = Integer.parseInt(fields[1]);
+		  String title = fields[2];
+		  
+		  ctx.write(new JoinKey(id, JoinKey.BOOK_TAG), new Text(title + "\t" + year));
+	  }
+  }
+  
+  static class JoinPartinioner extends Partitioner<JoinKey, Text> {
+	  
+	  @Override
+	  public int getPartition(JoinKey key, Text value, int numPartitions) {
+		  return (key.getId() & Integer.MAX_VALUE) % numPartitions;
+	  }
+  }
+  
+  static class JoinGroupingComparator extends WritableComparator {
+
+	protected JoinGroupingComparator() {
+		super(JoinKey.class, true);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Override
+	public int compare(WritableComparable key1, WritableComparable key2) {
+		JoinKey jK1 = (JoinKey) key1;
+		JoinKey jK2 = (JoinKey) key2;
+		
+		return (new Integer(jK1.getId())).compareTo(new Integer(jK2.getId()));
+	}
+	  
+  }
+  
+  static class JoinReducer extends Reducer<JoinKey, Text, Text, Text> {
+	  
+	  @Override
+	  protected void reduce(JoinKey key, Iterable<Text> values, Context ctx) throws java.io.IOException ,InterruptedException {
+		  //int id = key.getId();
+		  boolean isFirst = true;
+		  Text author = new Text("Unknown");
+		  
+		  for (Text value : values) {
+			  if (isFirst) {
+				  author = new Text(value);
+				  isFirst = false;
+			  }
+			  else {
+				  ctx.write(author, value);
+			  }
+		  }
+	  }
+  }
+  
+  static class JoinKey implements WritableComparable<JoinKey> {
+
+	  private int id;
+	  private int tag;
+	  
+	  public static final int AUTHOR_TAG = 0;
+	  public static final int BOOK_TAG = 1;
+	  public static final int NO_TAG = -1;
+	  
+	  public JoinKey() {
+		  id = -1;
+		  tag = NO_TAG;
+	  }
+	  
+	  public JoinKey(int id, int tag) {
+		  this.id = id;
+		  this.tag = tag;
+	  }
+	  
+	  public int getId() {
+		  return id;
+	  }
+	  
+	  public int getTag() {
+		  return tag;
+	  }	  
+	  
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		id = in.readInt();
+		tag = in.readInt();
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		out.writeInt(id);
+		out.writeInt(tag);
+	}
+
+	@Override
+	public int compareTo(JoinKey other) {
+		return ComparisonChain.start().compare(getId(), other.getId()).compare(getTag(), other.getTag()).result();
+	}
+	
+	@Override
+	public String toString() {
+		return id + "\t" + tag;
+	}
+	
+	@Override
+	public int hashCode() {
+		return toString().hashCode();
+	}
   }
 }
